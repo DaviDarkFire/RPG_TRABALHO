d9fed42f75b561c10744103eca189554465e3b40
Davi <daviferreirasantog@gmail.com>
2015-06-09 22:39:39 -0400

    leitura refeita com função

--- trab2prog.c
+++ trab2prog.c
@@ -8,8 +8,8 @@ typedef struct {
   } dados;
 typedef struct {
     char nome[MAX+1];
-    /* represente aqui de alguma forma a raça */
-    /* represente aqui de alguma forma a classe */
+    char raca[3];
+    char classe[3];
     int NVL;
     int FOR;
     int DES;
@@ -21,18 +21,38 @@ typedef struct {
   } personagem;
 
 
+
+void ler_personagens(void){
+  int nivel1, nivel2;
+  char nome1[MAX+1], nome2[MAX+1], classe1[3], raca1[3], classe2[3], raca2[3], x;
+
+  scanf(" %s %s %s %d%*c", nome1, raca1, classe1, &nivel1);
+  scanf("%c%*c", &x);
+  scanf(" %s %s %s %d%*c", nome2, raca2, classe2, &nivel2);
+
+  printf("%s %s %s %d\n", nome1, raca1, classe1, nivel1);
+  printf("%s %s %s %d\n", nome2, raca2, classe2, nivel2);
+
+
+
+  }
+
+
+
+
+
+
+
 int main(void){
-  int k, nivel, nivel2;
-  char nome[MAX], nome2[MAX], raca[2], raca2[2], classe[2], classe2[2], x;
+  int k;
   scanf("%d", &k);
   while(k>0){
+    ler_personagens();
 
-    scanf("%s,%s,%s,%d", nome, raca, classe, &nivel);
-    scanf("%c", &x);
-    scanf("%s,%s,%s,%d", nome2, raca2, classe2, &nivel2);
 
-    printf("%s %s %s %d\n", nome, raca, classe, nivel);
-    printf("%s %s %s %d\n", nome2, raca2, classe2, nivel2);
+
+
+
 
 
 
